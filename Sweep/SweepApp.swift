a885420c6f1345ca8ca3274df93e8eb51bc959bc
Renato Martins <rochamartinsrenato@gmail.com>
2023-11-27 08:07:20 +0000

    Initial Commit

---
+++ Sweep/SweepApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweepApp.swift
+//  Sweep
+//
+//  Created by Renato Martins on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SweepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
